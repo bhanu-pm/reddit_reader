ab45f6cd66c03c959aa782e9299a4472f043d8c6
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-07-27 14:39:03 -0700

    Implemented secrets with environment variables

--- read_comments.py
+++ read_comments.py
@@ -1,1 +1,18 @@
-hi = True 
+import os
+from dotenv import load_dotenv
+from praw import Reddit
+
+
+# Reddit user object
+load_dotenv()
+reddit = Reddit(
+	client_id=os.getenv("CLIENT_ID"),
+	client_secret=os.getenv("CLIENT_SECRET"),
+	user_agent="reddit_reader by user")
+
+URL = "https://www.reddit.com/r/postmates/comments/1dsmnm3/monthly_existing_user_promo_code_thread/"
+post = reddit.submission(url = URL)
+
+
+
+
