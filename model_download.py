f4e8950687f093001fbb51a5f1673f3f4a04a778
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-07-27 21:24:01 -0700

    updated save path for weights

--- model_download.py
+++ model_download.py
@@ -8,8 +8,8 @@ MODEL_NAME = "Qwen/Qwen2-1.5B-Instruct"
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.float16, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-model.save_pretrained("/weights")
-tokenizer.save_pretrained("/weights")
+model.save_pretrained("./weights")
+tokenizer.save_pretrained("./weights")
 
 # Removing model weights and cache from device gpu
 del model, tokenizer
