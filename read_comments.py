c5e74dfb1089a6cd2ab2438d9e44f15daaaa4088
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-07-27 19:48:43 -0700

    Extracts all comments, sorts by new

--- read_comments.py
+++ read_comments.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
+import time
+from datetime import datetime
+
 from praw import Reddit
+from praw.models import MoreComments
 
 
 # Reddit user object
@@ -12,8 +16,25 @@ reddit = Reddit(
 
 URL = str(os.getenv("TARGET_URL"))
 
+# function to extract time from a 2d array
+def sorter(my_list):
+	return my_list[0]
+
+
+# Reading the target post and its comments
 post = reddit.submission(url = URL)
+top_level_comments_obj = post.comments
 
+top_level_comments = []
+for comment in top_level_comments_obj:
+	top_level_comments.append([comment.created_utc, comment.body])
 
+# Sorting top level comments in order of newest first
+top_level_comments.sort(reverse=True, key=sorter)
 
+for comment in top_level_comments:
+	created_time_utc = datetime.utcfromtimestamp(comment[0] - 25200).strftime('%Y-%m-%d %H:%M:%S')
+	print(created_time_utc)
+	print(comment[1])
+	print("-"*20 + "\n")
 
