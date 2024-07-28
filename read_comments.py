38e6f5c77384574832e8bc37925761428ca237a9
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-07-28 15:33:59 -0700

    updated read_comments & created storage json

--- read_comments.py
+++ read_comments.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import time
 from datetime import datetime
+import json
 
 from praw import Reddit
 from praw.models import MoreComments
@@ -17,8 +18,8 @@ reddit = Reddit(
 URL = str(os.getenv("TARGET_URL"))
 
 # function to extract time from a 2d array
-def sorter(my_list):
-	return my_list[0]
+def sorter(my_dict):
+	return my_dict['time']
 
 
 # Reading the target post and its comments
@@ -26,15 +27,40 @@ post = reddit.submission(url = URL)
 top_level_comments_obj = post.comments
 
 top_level_comments = []
-for comment in top_level_comments_obj:
-	top_level_comments.append([comment.created_utc, comment.body])
+for comment in top_level_comments_obj: # -25200 to change time zone to MST
+	top_level_comments.append({'time':int(comment.created_utc)-25200, 'body':comment.body, 'replies':comment.replies})
 
 # Sorting top level comments in order of newest first
 top_level_comments.sort(reverse=True, key=sorter)
 
-for comment in top_level_comments:
-	created_time_utc = datetime.utcfromtimestamp(comment[0] - 25200).strftime('%Y-%m-%d %H:%M:%S')
-	print(created_time_utc)
-	print(comment[1])
-	print("-"*20 + "\n")
+# Retrieving previously used comments to compare and find unseen latest comments
+try:
+	used_comments_obj = open('used_comments_file.json')
+	used_comments_json = json.load(used_comments_obj)
+	used_comments = used_comments_json['used_comments']
+	used_comments_obj.close()
+except:
+	# print('Empty file')
+	used_comments = None
 
+# Inserting the latest comments in the to_be_used list, in order of oldest first
+to_be_used = []
+
+if used_comments is not None:
+	for new_comment in top_level_comments:
+		if new_comment['time'] > used_comments[0]['time']:
+			to_be_used.insert(0, new_comment)
+		else:
+			break
+elif used_comments is None:
+	to_be_used.insert(0, new_comment)
+
+# Sorted in the order of latest/newest first, joined at start to previously used comments
+now_used = to_be_used.copy()
+now_used.sort(reverse=True, key=sorter)
+now_used.extend(used_comments)
+
+used = {'used_comments': now_used}
+
+with open('used_comments_file.json', 'w') as used_comments_file:
+	json.dump(used, used_comments_file)
